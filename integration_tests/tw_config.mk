1d7282ced6c29bb166c6c6d1ed1d4c9405e276bb
Tobias Wicky <twicki@ethz.ch>
2019-04-30 15:46:23 +0200

    remove unneccessary file

--- integration_tests/tw_config.mk
+++
@@ -1,5 +0,0 @@
-GRIDTOOLS   = -DBACKEND_MC -I/home/tobias/Documents/work/clang-gridtools/bundle/build/prefix/gridtools/include/
-GTCLANG     = -isystem /home/tobias/Documents/work/gtclang/bundle/install/include
-BOOST       = -isystem /home/tobias/mylibs/boost_install/include
-DAPP        = -I/home/tobias/Documents/MeteoSwiss/ESCAPE/DAPP_Collaboration/dappy/dace/runtime/include -std=c++14
-COMPILER    = g++
