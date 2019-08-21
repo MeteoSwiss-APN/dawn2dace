311bc2a327351a60feb6ea9764a1e288fcdbf9cc
Tobias Wicky <twicki@ethz.ch>
2019-08-21 10:08:30 +0200

    fixit

--- cmake/scripts/compileProto.sh
+++ cmake/scripts/compileProto.sh
@@ -7,5 +7,5 @@ binary_dir=$3
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${binary_dir}/prefix/protobuf/lib/
 mkdir -p ${binary_dir}/gen
 ${proto_binary} --python_out=${binary_dir}/gen ${source_dir}/iir_specification/IIR.proto -I=${source_dir} -I=${source_dir}/iir_specification/ > /tmp/log
-${proto_binary} --python_out=${binary_dir}/gen ${source_dir}/iir_specification/statements.proto -I=${source_dir} > /tmp/log
+# ${proto_binary} --python_out=${binary_dir}/gen ${source_dir}/iir_specification/statements.proto -I=${source_dir} > /tmp/log
 
