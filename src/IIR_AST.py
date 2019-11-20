475b658fd3945d8a6e5eb2a71a22484dbdcc48fa
Dominic Hofer <dohofer86@gmail.com>
2019-11-20 15:02:03 +0100

    Implements a better IIR AST visitor and a transformer on top.

--- src/IIR_AST.py
+++ src/IIR_AST.py
@@ -1,5 +1,48 @@
 import IIR_pb2
 
+def DowncastStmt(stmt):
+    which = stmt.WhichOneof("stmt")
+    if which == "block_stmt":
+        return stmt.block_stmt
+    if which == "expr_stmt":
+        return stmt.expr_stmt
+    if which == "var_decl_stmt":
+        return stmt.var_decl_stmt
+    if which == "if_stmt":
+        return stmt.if_stmt
+    raise ValueError("Unexpected statement: " + which)
+
+def DowncastExpr(expr):
+    which = expr.WhichOneof("expr")
+    if which == "unary_operator":
+        return expr.unary_operator
+    if which == "binary_operator":
+        return expr.binary_operator
+    if which == "assignment_expr":
+        return expr.assignment_expr
+    if which == "ternary_operator":
+        return expr.ternary_operator
+    if which == "fun_call_expr":
+        return expr.fun_call_expr
+    if which == "var_access_expr":
+        return expr.var_access_expr
+    if which == "field_access_expr":
+        return expr.field_access_expr
+    if which == "literal_access_expr":
+        return expr.literal_access_expr
+    raise ValueError("Unexpected expression: " + which)
+
+def iter_fields(node):
+    """
+    Yield a tuple of ``(fieldname, value)`` for each field in ``node._fields``
+    that is present on *node*.
+    """
+    for field in dir(node):
+        try:
+            yield field, getattr(node, field)
+        except AttributeError:
+            pass
+
 def _qualname(obj):
     """Get the fully-qualified name of an object (including module)."""
     return obj.__module__ + '.' + obj.__qualname__
@@ -36,130 +79,113 @@ def visitor(arg_type):
 
     return decorator
 
-def DowncastStmt(stmt):
-    which = stmt.WhichOneof("stmt")
-    if which == "block_stmt":
-        return stmt.block_stmt
-    if which == "expr_stmt":
-        return stmt.expr_stmt
-    if which == "var_decl_stmt":
-        return stmt.var_decl_stmt
-    if which == "if_stmt":
-        return stmt.if_stmt
-    raise ValueError("Unexpected statement: " + which)
-
-def DowncastExpr(expr):
-    which = expr.WhichOneof("expr")
-    if which == "unary_operator":
-        return expr.unary_operator
-    if which == "binary_operator":
-        return expr.binary_operator
-    if which == "assignment_expr":
-        return expr.assignment_expr
-    if which == "ternary_operator":
-        return expr.ternary_operator
-    if which == "fun_call_expr":
-        return expr.fun_call_expr
-    if which == "var_access_expr":
-        return expr.var_access_expr
-    if which == "field_access_expr":
-        return expr.field_access_expr
-    if which == "literal_access_expr":
-        return expr.literal_access_expr
-    raise ValueError("Unexpected expression: " + which)
+IIR_Classes = (
+    IIR_pb2.SIR_dot_statements__pb2.Stmt,
+    IIR_pb2.SIR_dot_statements__pb2.Expr,
+    IIR_pb2.SIR_dot_statements__pb2.BlockStmt,
+    IIR_pb2.SIR_dot_statements__pb2.ExprStmt,
+    IIR_pb2.SIR_dot_statements__pb2.ReturnStmt,
+    IIR_pb2.SIR_dot_statements__pb2.VarDeclStmt,
+    IIR_pb2.SIR_dot_statements__pb2.StencilCallDeclStmt,
+    IIR_pb2.SIR_dot_statements__pb2.VerticalRegionDeclStmt,
+    IIR_pb2.SIR_dot_statements__pb2.BoundaryConditionDeclStmt,
+    IIR_pb2.SIR_dot_statements__pb2.IfStmt,
+    IIR_pb2.SIR_dot_statements__pb2.UnaryOperator,
+    IIR_pb2.SIR_dot_statements__pb2.BinaryOperator,
+    IIR_pb2.SIR_dot_statements__pb2.AssignmentExpr,
+    IIR_pb2.SIR_dot_statements__pb2.TernaryOperator,
+    IIR_pb2.SIR_dot_statements__pb2.FunCallExpr,
+    IIR_pb2.SIR_dot_statements__pb2.StencilFunCallExpr,
+    IIR_pb2.SIR_dot_statements__pb2.StencilFunArgExpr,
+    IIR_pb2.SIR_dot_statements__pb2.VarAccessExpr,
+    IIR_pb2.SIR_dot_statements__pb2.FieldAccessExpr,
+    IIR_pb2.SIR_dot_statements__pb2.LiteralAccessExpr,
+    IIR_pb2.SIR_dot_statements__pb2.ReductionOverNeighborExpr
+)
 
 class IIR_Visitor:
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.Stmt)
-    def visit(self, stmt):
+    def visit(self, node):
+        """Visit a node."""
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(node)
+
+    def visit_Stmt(self, stmt):
         self.visit(DowncastStmt(stmt))
 
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.Expr)
-    def visit(self, expr):
+    def visit_Expr(self, expr):
         self.visit(DowncastExpr(expr))
 
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.BlockStmt)
-    def visit(self, stmt):
-        for s in stmt.statements:
-            self.visit(s)
+    def generic_visit(self, node):
+        """Called if no explicit visitor function exists for a node."""
+        for field, value in iter_fields(node):
+            if field.startswith('__weakref__'):
+                continue
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item, IIR_Classes):
+                        self.visit(item)
+            elif isinstance(value, IIR_Classes):
+                self.visit(value)
 
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.ExprStmt)
-    def visit(self, stmt):
-        self.visit(stmt.expr)
 
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.ReturnStmt)
-    def visit(self, stmt):
-        self.visit(stmt.expr)
+class IIR_Transformer(IIR_Visitor):
+    def visit_Stmt(self, stmt):
+        which = stmt.WhichOneof("stmt")
+        if which == "block_stmt":
+            stmt.block_stmt.CopyFrom(self.visit(stmt.block_stmt))
+        elif which == "expr_stmt":
+            stmt.expr_stmt.CopyFrom(self.visit(stmt.expr_stmt))
+        elif which == "var_decl_stmt":
+            stmt.var_decl_stmt.CopyFrom(self.visit(stmt.var_decl_stmt))
+        elif which == "if_stmt":
+            stmt.if_stmt.CopyFrom(self.visit(stmt.if_stmt))
+        else:
+            raise ValueError("Unexpected stmt: " + which)
+        return stmt
 
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.VarDeclStmt)
-    def visit(self, stmt):
-        for expr in stmt.init_list:
-            self.visit(expr)
+    def visit_Expr(self, expr):
+        which = expr.WhichOneof("expr")
+        if which == "unary_operator":
+            expr.unary_operator.CopyFrom(self.visit(expr.unary_operator))
+        elif which == "binary_operator":
+            expr.binary_operator.CopyFrom(self.visit(expr.binary_operator))
+        elif which == "assignment_expr":
+            expr.assignment_expr.CopyFrom(self.visit(expr.assignment_expr))
+        elif which == "ternary_operator":
+            expr.ternary_operator.CopyFrom(self.visit(expr.ternary_operator))
+        elif which == "fun_call_expr":
+            expr.fun_call_expr.CopyFrom(self.visit(expr.fun_call_expr))
+        elif which == "var_access_expr":
+            expr.var_access_expr.CopyFrom(self.visit(expr.var_access_expr))
+        elif which == "field_access_expr":
+            expr.field_access_expr.CopyFrom(self.visit(expr.field_access_expr))
+        elif which == "literal_access_expr":
+            expr.literal_access_expr.CopyFrom(self.visit(expr.literal_access_expr))
+        else:
+            raise ValueError("Unexpected expr: " + which)
+        return expr
 
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.StencilCallDeclStmt)
-    def visit(self, stmt):
-        pass
-
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.VerticalRegionDeclStmt)
-    def visit(self, stmt):
-        pass
-
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.BoundaryConditionDeclStmt)
-    def visit(self, stmt):
-        pass
-
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.IfStmt)
-    def visit(self, stmt):
-        self.visit(stmt.cond_part)
-        self.visit(stmt.then_part)
-        self.visit(stmt.else_part)
-
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.UnaryOperator)
-    def visit(self, stmt):
-        self.visit(stmt.operand)
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.BinaryOperator)
-    def visit(self, stmt):
-        self.visit(stmt.left)
-        self.visit(stmt.right)
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.AssignmentExpr)
-    def visit(self, expr):
-        self.visit(expr.left)
-        self.visit(expr.right)
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.TernaryOperator)
-    def visit(self, stmt):
-        self.visit(stmt.cond)
-        self.visit(stmt.left)
-        self.visit(stmt.right)
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.FunCallExpr)
-    def visit(self, expr):
-        for arg in expr.arguments:
-            self.visit(arg)
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.StencilFunCallExpr)
-    def visit(self, expr):
-        for arg in expr.arguments:
-            self.visit(arg)
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.StencilFunArgExpr)
-    def visit(self, expr):
-        pass
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.VarAccessExpr)
-    def visit(self, expr):
-        pass
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.FieldAccessExpr)
-    def visit(self, expr):
-        pass
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.LiteralAccessExpr)
-    def visit(self, expr):
-        pass
-    
-    @visitor(IIR_pb2.SIR_dot_statements__pb2.ReductionOverNeighborExpr)
-    def visit(self, expr):
-        self.visit(expr.rhs)
-        self.visit(expr.init)
+    def generic_visit(self, node):
+        for field, old_value in iter_fields(node):
+            if field.startswith('__weakref__'):
+                continue
+            if isinstance(old_value, list):
+                new_values = []
+                for value in old_value:
+                    if isinstance(value, IIR_Classes):
+                        value = self.visit(value)
+                        if value is None:
+                            continue
+                        elif not isinstance(value, IIR_Classes):
+                            new_values.extend(value)
+                            continue
+                    new_values.append(value)
+                old_value[:] = new_values
+            elif isinstance(old_value, IIR_Classes):
+                new_node = self.visit(old_value)
+                if new_node is None:
+                    delattr(node, field)
+                else:
+                    old_value.CopyFrom(new_node)
+        return node
