5d46e6c52ce21f48630b0bf2471f78d21d242fe2
Dominic Hofer <dohofer86@gmail.com>
2019-11-26 16:59:11 +0100

    Hacks coriolis to be 3D arrays only.

--- src/natural_stencils/coriolis.cpp
+++ src/natural_stencils/coriolis.cpp
@@ -5,7 +5,7 @@ using namespace gridtools::clang;
 stencil coriolis_stencil {
   storage u_tens, v_tens;
   storage u, v;
-  storage_ij fc;
+  storage fc;
 
   Do() {
     vertical_region(k_start, k_end) {
