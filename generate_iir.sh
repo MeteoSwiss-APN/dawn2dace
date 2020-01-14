bb6266f17c3a32965aa98527700ce37f440a8ad0
Dominic Hofer <dohofer86@gmail.com>
2020-01-14 16:26:34 +0100

    Uses clang-gridtools' dycore as source.

--- generate_iir.sh
+++ generate_iir.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 GTCLANG=/home/dominic/work/dawn/gtclang/bundle/install/bin/gtclang
 
-FILES=src/{synthetic_stencils,natural_stencils}/*.cpp
-
 for fullfile in src/*_stencils/*.cpp
+do
+	echo "Processing $fullfile ..."
+	filename="${fullfile##*/}"
+	$GTCLANG $fullfile -fwrite-iir -iir-format=byte -fdebug -fpartition-intervals -o gen/$filename
+done
+
+
+for fullfile in ../clang-gridtools/src/dycore/*.cpp
 do
 	echo "Processing $fullfile ..."
 	filename="${fullfile##*/}"
